025471f42005efaad4b4dddab09e9fab7776ad60
gourav13 <gourav13041997@gmail.com>
2019-09-22 11:12:25 +0530

    eight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
-import { AlertModule} from 'ngx-bootstrap';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { SchoolxprodersComponent } from './schoolxproders/schoolxproders.component';
 import { NavmainComponent } from './navmain/navmain.component';
